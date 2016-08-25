45ce39ffb660c681dc34f11838c79cd5ca8e18ea
youpy <youpy_yagi0311@yahoo.co.jp>
2016-08-25 04:21:00 +0000

    gnan

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-100.times do |n|
-  email = Faker::Internet.email
-  name = Faker::Internet.name
-  password = "password"
-  User.create!(email: email,
-               password: password,
-               password_confirmation: password,
-               name: name,
-               )
-end
+#100.times do |n|
+#  email = Faker::Internet.email
+#  name = Faker::Internet.name
+#  password = "password"
+#  User.create!(email: email,
+#               password: password,
+#               password_confirmation: password,
+#               name: name,
+#               )
+#end
 
 
 10.times do |n|
-	rand_id = 1
+	rand_id = 0
 	while User.find(rand_id) != nil do
-		rand_id = rand(1..1000)
+		rand_id = rand(1..1500)
 	end	
 	title = "heroku#{n+1}"
 	content = "ないよう#{n+1}"
