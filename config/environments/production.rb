8cdca60621aeddd486ab6c6a6c60a9379f6b6326
youpy <youpy_yagi0311@yahoo.co.jp>
2016-08-25 02:44:31 +0000

    adeawh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
- user_name: ENV['app55517951@heroku.com'],
- password: ENV['2dnil8jv5159'],
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD'],
  domain: "heroku.com",
  address: "smtp.sendgrid.net",
  port: 587,
