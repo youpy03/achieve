331eb5d35bcf16aef200dba04b26bd19c6c00d48
youpy <youpy_yagi0311@yahoo.co.jp>
2016-08-25 06:47:48 +0000

    dsfdf

--- db/migrate/20160823200511_devise_create_users.rb
+++ db/migrate/20160823200511_devise_create_users.rb
@@ -19,6 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.inet     :current_sign_in_ip
       t.inet     :last_sign_in_ip
 
+      #ここいるのか???
       ## Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
